8dac3c13e6ea7e5c13aa5c56de460f8a3b0e67c9
OMEN <OMEN@DESKTOP-COQB2HO>
2022-03-02 17:57:11 -0600

    Controller y repositories

--- src/main/java/com/Tienda/controller/PersonaController.java
+++ src/main/java/com/Tienda/controller/PersonaController.java
@@ -1,31 +1,41 @@
-
 package com.Tienda.controller;
 
 import com.Tienda.entity.Persona;
 import com.Tienda.service.IPersonaService;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-
+@Slf4j
 @Controller
 public class PersonaController {
-    
-@Autowired
-private IPersonaService personaService;
 
+    @Autowired
+    private IPersonaService personaService;
 
-@GetMapping("/personas")
-public String index(Model model){
+    @GetMapping("/personas")
+    public String index(Model model) {
 
-List<Persona> listaPersonas=personaService.getAllPerson();
-model.addAttribute("titulo","Personas");
-model.addAttribute("personas",listaPersonas);
-return "personas";
+        List<Persona> listaPersonas = personaService.getAllPerson();
+        model.addAttribute("titulo", "Personas");
+        model.addAttribute("personas", listaPersonas);
+        return "personas";
 
-}
+    }
 
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Persona persona) {
+        return "modificarCliente";
+    }
+
+    @PostMapping("/guardarCliente")
+    public String guardarPersona(Persona persona) {
+        personaService.savePerson(persona);
+        return "redirect:/";
+    }
 
 }
